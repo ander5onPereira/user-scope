1482c563f726260ca7ea5f86e4c63ad3f4081eda
Anderson Pereira <anderson.carro.95@gmail.com>
2025-10-14 23:07:11 -0300

    chore: config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default defineConfig([
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ]);
