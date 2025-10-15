b691d60f2bb225e99b100891d6199b57b1802cb3
Anderson Pereira <anderson.carro.95@gmail.com>
2025-10-15 14:30:35 -0300

    fix: build configuration by manually splitting larger packages to improve application build

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          i18n: ['i18next', 'react-i18next'],
+        },
+      },
+    },
+  },
 })
